b19d192b5e92924c715198dba19cba954ae877d5
indrajit <palindrajit10@gmail.com>
2021-12-03 12:55:32 +0530

    Program4 method 2

--- Assignment 8/Program4.py
+++ Assignment 8/Program4.py
@@ -1,4 +1,5 @@
-def string_test(s):
+#Method 1
+def string(s):
     dict = {"UPPER_CASE": 0, "LOWER_CASE": 0}
     for c in s:
         if c.isupper():
@@ -11,4 +12,22 @@ def string_test(s):
     print ("No. of Upper case characters : ", dict["UPPER_CASE"])
     print ("No. of Lower case Characters : ", dict["LOWER_CASE"])
 Sample_String = "The quick Brown Fox"
-string_test(Sample_String)
+string(Sample_String)
+
+
+#Method 2
+def string(s):
+    upper = 0
+    lower = 0
+    for c in s:
+        if c.isupper():
+           upper = upper + 1
+        elif c.islower():
+            lower = lower + 1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", upper)
+    print ("No. of Lower case Characters : ", lower)
+Sample_String = "The quick Brown Fox"
+string(Sample_String)
