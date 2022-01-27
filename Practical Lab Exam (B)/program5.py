d4ae6cb6f30794ee291f8e40bbe81e777ce01a47
indrajit <palindrajit10@gmail.com>
2022-01-27 14:31:56 +0530

    program 5

---
+++ Practical Lab Exam (B)/program5.py
@@ -0,0 +1,15 @@
+# Q5. Write a Python program to find the length and index of an item of a tuple.
+
+def func(item, tup):
+    for i in range(len(tup)):
+        if tup[i] == item:
+            return i + 1
+    return -1
+
+tuple = ()
+x = int(input("Enter the number of elements in the tuple: "))
+for i in range(x):
+    tuple += (input("Enter element: "),)
+# print(tuple)
+item = input("Enter the item to find: ")
+print(f"Length of item is {len(item)} and index is {func(item, tuple)}")
